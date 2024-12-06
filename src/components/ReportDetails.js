7e3e0afb00da1c6630ccdfedc6f53bf3861894da
PikuzaMarie <pikuza.mari@gmail.com>
2024-12-06 14:36:24 +0300

    style: style report page

--- src/components/ReportDetails.js
+++ src/components/ReportDetails.js
@@ -1,16 +1,21 @@
 import React, { useEffect } from "react";
-import { Box, Typography, Button, Breadcrumbs, Link } from "@mui/material";
 import { useParams, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchReportByProjectId } from "../store/reportsSlice";
 import MainHeader from "./MainHeader";
+import { Box, Typography, IconButton, Breadcrumbs, Link } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useTheme } from "@mui/material/styles";
 
 const ReportDetails = () => {
+	const theme = useTheme();
 	const { projectId } = useParams();
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
 
 	const { reports, status, error } = useSelector((state) => state.reports);
+
+	const employeeName = useSelector((state) => state.employee?.name);
 
 	const report = reports.find((r) => r.projectid === parseInt(projectId, 10));
 
@@ -19,6 +24,14 @@ const ReportDetails = () => {
 			dispatch(fetchReportByProjectId(projectId));
 		}
 	}, [projectId, report, dispatch]);
+
+	const formatDate = (date) => {
+		if (!date) return "";
+		const formattedDate = new Date(date);
+		return formattedDate instanceof Date && !isNaN(formattedDate)
+			? formattedDate.toISOString().split("T")[0]
+			: "";
+	};
 
 	if (status === "loading") {
 		return <Typography>Loading report...</Typography>;
@@ -35,54 +48,97 @@ const ReportDetails = () => {
 	return (
 		<Box>
 			<MainHeader />
-			<Breadcrumbs aria-label="breadcrumb">
-				<Link
-					underline="hover"
-					color="inherit"
-					href="/home">
-					Main
-				</Link>
-				<Link
-					underline="hover"
-					color="inherit"
-					href="/home/projects">
-					Projects
-				</Link>
-				<Link
-					underline="hover"
-					color="inherit"
-					href={`/home/projects/${projectId}`}>
-					Project
-				</Link>
-				<Typography sx={{ color: "text.primary" }}>
+			<Box
+				sx={{
+					padding: "24px 32px",
+				}}>
+				<Breadcrumbs aria-label="breadcrumb">
+					<Link
+						underline="hover"
+						color="inherit"
+						href="/home">
+						Main
+					</Link>
+					<Link
+						underline="hover"
+						color="inherit"
+						href="/home/projects">
+						Projects
+					</Link>
+					<Link
+						underline="hover"
+						color="inherit"
+						href={`/home/projects/${projectId}`}>
+						Project
+					</Link>
+					<Typography sx={{ color: theme.palette.black.main }}>
+						Report
+					</Typography>
+				</Breadcrumbs>
+				<Box
+					sx={{
+						display: "flex",
+						alignItems: "center",
+						gap: "10px",
+						marginTop: "40px",
+					}}>
+					<IconButton
+						onClick={() => navigate(`/home/projects/${projectId}`)}
+						edge="start"
+						aria-label="back">
+						<ArrowBackIcon />
+					</IconButton>
+					<Typography
+						variant="h4"
+						component="h3"
+						fontWeight="500">
+						Report Details
+					</Typography>
+				</Box>
+			</Box>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					gap: "16px",
+					padding: "0 32px",
+					width: "50vw",
+				}}>
+				<Typography
+					variant="h5"
+					sx={{ fontWeight: "600" }}>
 					{report.reportname}
 				</Typography>
-			</Breadcrumbs>
-			<Box sx={{ padding: "16px 32px" }}>
-				<Typography
-					variant="h4"
-					gutterBottom>
-					Report Details
-				</Typography>
-				<Typography variant="body1">
-					<strong>Report Name:</strong> {report.reportname}
-				</Typography>
-				<Typography variant="body1">
-					<strong>Total Cost: </strong> {report.totalcost} $
-				</Typography>
-				<Typography variant="body1">
-					<strong>Created Date:</strong> {report.reportcreateddate}
-				</Typography>
-				<Typography variant="body1">
-					<strong>Author:</strong> {report.reportauthor}
-				</Typography>
-				<Button
-					variant="outlined"
-					color="primary"
-					sx={{ marginTop: "16px" }}
-					onClick={() => navigate(`/home/projects/${projectId}`)}>
-					Back to Project
-				</Button>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "column",
+						paddingBottom: "16px",
+						borderTop: "1px solid",
+						borderBottom: "1px solid",
+						borderColor: theme.palette.custom.gray700,
+					}}>
+					<Typography
+						variant="h4"
+						sx={{ fontWeight: "700", marginTop: "8px" }}>
+						<strong>Calculated cost: </strong> {report.totalcost} $
+					</Typography>
+					<Typography
+						variant="h6"
+						sx={{ fontWeight: "700", marginTop: "8px" }}>
+						<strong>Budget:</strong> {report.budget} $
+					</Typography>
+				</Box>
+				<Box sx={{ marginTop: "16px", display: "flex", gap: "20px" }}>
+					<Typography variant="body1">
+						<strong>Created date: </strong>
+						{formatDate(report.reportcreateddate)}
+					</Typography>
+					<Typography variant="body1">
+						<strong>Author: </strong>
+						{employeeName}
+					</Typography>
+				</Box>
 			</Box>
 		</Box>
 	);
