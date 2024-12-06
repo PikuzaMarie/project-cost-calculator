c52666d132c99547e4dd6aa523cdbe945c3f15df
PikuzaMarie <pikuza.mari@gmail.com>
2024-12-06 14:11:12 +0300

    style: style project component

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Button, Box, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
 
 const ProjectDetails = ({
 	projectName,
 	clientName,
+	cost,
 	projectStatus,
 	projectId,
 	onEditProject,
 	onDeleteProject,
 }) => {
+	const theme = useTheme();
 	const navigate = useNavigate();
 
 	const handleCreateReport = () => {
@@ -19,35 +22,70 @@ const ProjectDetails = ({
 	return (
 		<Box
 			sx={{
-				borderBottom: "1px solid #ccc",
-				paddingBottom: "16px",
-				marginBottom: "16px",
+				display: "flex",
+				flexDirection: "row",
+				justifyContent: "space-between",
+				alignItems: "center",
+				width: "645px",
+				padding: "24px",
+				boxShadow: 3,
+				"&:hover": {
+					backgroundColor: theme.palette.background.main,
+				},
+				cursor: "pointer",
 			}}>
-			<Typography variant="h6">{projectName}</Typography>
-			<Typography variant="body2">Client: {clientName}</Typography>
-			<Typography variant="body2">Status: {projectStatus}</Typography>
+			<Box>
+				<Typography variant="h6">{projectName}</Typography>
+				<Box>
+					<Typography
+						variant="body2"
+						sx={{ color: theme.palette.custom.gray }}>
+						Client: {clientName}
+					</Typography>
+					<Typography
+						variant="body2"
+						sx={{
+							color:
+								projectStatus === "active"
+									? theme.palette.success.main
+									: theme.palette.error.main,
+						}}>
+						Status: {projectStatus}
+					</Typography>
+				</Box>
 
-			<Box sx={{ marginTop: "8px" }}>
-				<Button
-					onClick={handleCreateReport}
-					variant="contained"
-					color="primary"
-					sx={{ marginRight: "8px" }}>
-					Create Report
-				</Button>
-				<Button
-					onClick={onEditProject}
-					variant="outlined"
-					color="secondary"
-					sx={{ marginRight: "8px" }}>
-					View
-				</Button>
-				<Button
-					onClick={onDeleteProject}
-					variant="outlined"
-					color="error">
-					Delete
-				</Button>
+				<Box sx={{ display: "flex", gap: "8px", marginTop: "8px" }}>
+					<Button
+						onClick={handleCreateReport}
+						variant="contained"
+						disabled={projectStatus === "inactive"}>
+						Create Report
+					</Button>
+					<Button
+						onClick={onEditProject}
+						variant="outlined">
+						View
+					</Button>
+					<Button
+						onClick={onDeleteProject}
+						variant="outlinedError">
+						Delete
+					</Button>
+				</Box>
+			</Box>
+
+			<Box>
+				<Typography
+					variant="h4"
+					component="p"
+					sx={{
+						color:
+							cost > 0
+								? theme.palette.custom.black
+								: theme.palette.custom.gray700,
+					}}>
+					{cost} $
+				</Typography>
 			</Box>
 		</Box>
 	);
